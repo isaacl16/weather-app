c7cff51d7beb4e9f078fcb9d64e4825a65071b0f
root <root@DESKTOP-4UTI2RN.localdomain>
2022-09-14 02:45:55 +0800

    Initial Commit

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Body from "../components/body";
+import Footer from "../components/footer";
 import NavBar from "../components/navbar";
-
 const Layout = () => {
     return (
         <>
